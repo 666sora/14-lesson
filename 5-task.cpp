2ec2ea5de4327539a3259023aa42f362318efe8a
666sora <pavelbys03@gmail.com>
2022-04-06 01:42:21 +0700

    5-task done

--- 5-task.cpp
+++ 5-task.cpp
@@ -2,7 +2,20 @@
 #include <vector>
 
 int main() {
-    
+    float matrix[4][4];
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            std::cout << "Input number: ";
+            std::cin >> matrix[i][j];
+        }
+    }
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            if (i != j) matrix[i][j] = 0;
+            std::cout << matrix[i][j] << "\t";
+        }
+        std::cout << std::endl;
+    }
 }
 
 /*
