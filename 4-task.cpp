81b8f5b8521e3a4eeaba79ec6af5f553d7cf2399
666sora <pavelbys03@gmail.com>
2022-04-06 00:59:28 +0700

    4-task done without testing

--- 4-task.cpp
+++ 4-task.cpp
@@ -2,7 +2,26 @@
 #include <vector>
 
 int main() {
-    
+    int matrix[4][4];
+    std::cout << "Matrix 1:" << std::endl;
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            std::cout << "Input number: ";
+            std::cin >> matrix[i][j];
+        }
+    }
+    std::cout << "Matrix 2:" << std::endl;
+    bool equal = true;
+    for (int i = 0; i < 4 && equal; i++) {
+        for (int j = 0; j < 4 && equal; j++) {
+            std::cout << "Input number: ";
+            int temp;
+            std::cin >> temp;
+            equal = (temp == matrix[i][j] ? equal : false);
+        }
+    }
+    if (equal) std::cout << "Matrixes are equal";
+    else std::cout << "Matrixes are not equal";
 }
 
 /*
